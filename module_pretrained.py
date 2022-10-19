eccb07a3dd1f365052bdda2a05446670f820de20
zc <1361095044@qq.com>
2022-10-19 15:48:13 +0800

    pytorch学习第7天

---
+++ module_pretrained.py
@@ -0,0 +1,17 @@
+import torchvision.models
+from torch.nn import Linear
+# 加载现有的模型
+vgg16_false = torchvision.models.vgg16(pretrained=False)
+vgg16_true = torchvision.models.vgg16(pretrained=True)
+
+print(vgg16_true)
+# 添加新的
+# vgg16_true.add_module('add_linear', Linear(1000, 10))
+# print(vgg16_true)
+# 添加新的进内层
+vgg16_true.classifier.add_module('add_linear', Linear(1000, 10))
+print(vgg16_true)
+# 修改module
+print(vgg16_false)
+vgg16_false.classifier[6] = Linear(4096, 10)
+print(vgg16_false)
